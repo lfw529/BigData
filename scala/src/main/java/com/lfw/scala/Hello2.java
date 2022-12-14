86a3aef9be555c0b75ada33cb902e793eb3d0035
lfw529 <differentlee1012@163.com>
2022-12-14 21:19:35 +0800

    1111

---
+++ scala/src/main/java/com/lfw/scala/Hello2.java
@@ -0,0 +1,15 @@
+package com.lfw.scala;
+
+public class Hello2 {
+    public static void main(String[] args) {
+        Hello2$.MOUDLE$.main(args);
+    }
+}
+
+final class Hello2$ {
+    public static final Hello2$ MODULE$;
+
+    static {
+        MODULE$ = new Hello2$();
+    }
+}
